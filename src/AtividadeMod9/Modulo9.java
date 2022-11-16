57c39d8a80f2b89eae0f4e470422e68341253b4f
Gisela-Lopes <giselag_lopes@hotmail.com>
2022-11-15 22:51:24 -0300

    Commitando a tarefa do mod9 para o Github

---
+++ src/AtividadeMod9/Modulo9.java
@@ -0,0 +1,17 @@
+package AtividadeMod9;
+
+/**
+ * @author Gisela
+ */
+
+public class Modulo9 {
+
+ public static void main(String args[]) {
+
+     int ano = 2022;
+     long anol = ano;
+     System.out.println(anol);
+
+ }
+
+}
